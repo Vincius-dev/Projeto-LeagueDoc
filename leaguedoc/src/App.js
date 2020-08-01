18456c490c3bddad899b60eeccf685bb9c7cb3b6
Vincius-dev <vinicius.fariascontato@gmail.com>
2020-08-01 15:22:23 -0300

    Atualizações

--- leaguedoc/src/App.js
+++ leaguedoc/src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
 import dadosIniciais from './data/dados_iniciais.json';
-
+import Carousel from './components/Carousel';
 
 function App() {
   return (
-    <div>
+    <div style={{ background:"#141414"}}>
     <Menu/>
-    <h1>HELLO WORLD</h1>
-    AUTOMATICO hot reloud 
+
+    <BannerMain
+    videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+    url={dadosIniciais.categorias[0].videos[0].url}
+    videoDescription={"teste,teste,teste"}
+    />
+
+    <Carousel
+    ignoreFirstVideo
+    category={dadosIniciais.categorias[0]}
+    />
+
+    <Carousel
+    category={dadosIniciais.categorias[1]}
+    />
+
+    <Carousel
+    category={dadosIniciais.categorias[2]}
+    />
     </div>
   );
 }
