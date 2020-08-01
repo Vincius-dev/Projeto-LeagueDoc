88586b2f6cfcc7b4d5c0405c5e5518929b9db49d
Vincius-dev <vinicius.fariascontato@gmail.com>
2020-08-01 15:32:40 -0300

    Atualização

--- leaguedoc/src/App.js
+++ leaguedoc/src/App.js
@@ -3,6 +3,7 @@ import Menu from './components/Menu';
 import BannerMain from './components/BannerMain';
 import dadosIniciais from './data/dados_iniciais.json';
 import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -27,6 +28,20 @@ function App() {
     <Carousel
     category={dadosIniciais.categorias[2]}
     />
+
+    <Carousel
+    category={dadosIniciais.categorias[3]}
+    />
+
+    <Carousel
+    category={dadosIniciais.categorias[4]}
+    />
+
+    <Carousel
+    category={dadosIniciais.categorias[5]}
+    />
+
+    <Footer/>
     </div>
   );
 }
